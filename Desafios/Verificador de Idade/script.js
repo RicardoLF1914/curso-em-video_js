860262adb1c5f1ca32532f1f8577ebcfbf0a7a1f
Ricardo Linhares <ricardofilho887@gmail.com>
2026-02-24 20:05:14 -0300

    update .html and script

--- Desafios/Verificador de Idade/script.js
+++ Desafios/Verificador de Idade/script.js
@@ -1,26 +1,31 @@
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var f_ano = document.getElementById('txtano')
+    var hoje = new Date()
+    var f_data = document.getElementById('txtdata')
     var r = document.getElementById('resultado')
+    var nascimento = new Date(f_data.value)
 
-    if(f_ano.value.length == 0 || f_ano.value > ano) {
+    if (f_data.value.length == 0 || nascimento > hoje) {
         alert('[ERRO] Verifique os dados e tente novamente')
     } else {
+        var idade = hoje.getFullYear() - nascimento.getFullYear()
+
+        var aniversarioPassou = hoje.getMonth() > nascimento.getMonth() || (hoje.getMonth() == nascimento.getMonth() && hoje.getDate() >= nascimento.getDate())
+
+        if (!aniversarioPassou) idade--
+
         var fsex = document.getElementsByName('radsex')
-        var idade = ano - f_ano.value
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        
-        if(fsex[0].checked) {
+
+        if (fsex[0].checked) {
             genero = 'Homem'
 
-            if(idade >= 0 && idade < 12) {
+            if (idade >= 0 && idade < 12) {
                 img.setAttribute('src', './assets/bebe-m.jpg')
-            } else if(idade < 21) {
+            } else if (idade < 21) {
                 img.setAttribute('src', './assets/jovem-m.jpg')
-            } else if(idade < 60) {
+            } else if (idade < 60) {
                 img.setAttribute('src', './assets/adulto-m.jpg')
             } else {
                 img.setAttribute('src', './assets/idoso-m.jpg')
@@ -29,18 +34,18 @@ function verificar() {
         } else {
             genero = 'Mulher'
 
-            if(idade >= 0 && idade < 12) {
+            if (idade >= 0 && idade < 12) {
                 img.setAttribute('src', './assets/bebe-f.jpg')
-            } else if(idade < 21) {
+            } else if (idade < 21) {
                 img.setAttribute('src', './assets/jovem-f.jpg')
-            } else if(idade < 60) {
+            } else if (idade < 60) {
                 img.setAttribute('src', './assets/adulta-f.jpg')
             } else {
                 img.setAttribute('src', './assets/idosa-f.jpg')
             }
         }
 
-        r.innerHTML = `Detectamos: ${genero} com ${idade} anos`
+        r.innerHTML = `${genero} com ${idade} anos`
         r.appendChild(img)
     }
 }
